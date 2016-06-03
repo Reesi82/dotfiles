0c26cb1ac84ab8688784217d6203f4a628790e6e
Reesi82 <kristofnemeth12@hotmail.com>
2016-06-03 09:01:50 -0400

    Add Tmux Plugins

--- install/link.sh
+++ install/link.sh
@@ -32,6 +32,23 @@ for config in $DOTFILES/config/*; do
     fi
 done
 
+echo -e "\n\ninstalling to ~/.tmux"
+echo "=============================="
+if [ ! -d $HOME/.tmux ]; then
+    echo "Creating ~/.tmux"
+    mkdir -p $HOME/.tmux
+fi
+# configs=$( find -path "$DOTFILES/tmux.symlink" -maxdepth 1 )
+for tmux in $DOTFILES/tmux/config/*; do
+    target=$HOME/.tmux/$( basename $tmux )
+    if [ -e $target ]; then
+        echo "~${target#$HOME} already exists... Skipping."
+    else
+        echo "Creating symlink for $tmux"
+        ln -s $tmux $target
+    fi
+done
+
 # create vim symlinks
 # As I have moved off of vim as my full time editor in favor of neovim,
 # I feel it doesn't make sense to leave my vimrc intact in the dotfiles repo
@@ -54,3 +71,5 @@ for file in "${!vimfiles[@]}"; do
         ln -s ${vimfiles[$file]} $file
     fi
 done
+
+
